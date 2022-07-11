f198e51d37be44f17b53fb0ad19c1c3219800901
Mark <mcherepivskiy04@gmail.com>
2022-07-11 20:28:38 +0600

    fixes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
 	transpileDependencies: true,
 	lintOnSave: true,
-	publicPath: process.env.NODE_ENV === 'production' ? '/money-express/' : '/',
+	publicPath: '/money-express/',
 	devServer: {
 		proxy: 'https://api.cashu.kz/',
 	},
